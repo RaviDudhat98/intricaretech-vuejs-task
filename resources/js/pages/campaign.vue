c5fae407d85f8fe83fb894f6956f04ca25578408
RaviDudhat98 <ravidudhat98@gmail.com>
2026-03-21 12:42:18 +0530

    Initial commit with Vuejs theme starterkit.

---
+++ resources/js/pages/campaign.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <VCard title="Campaign Page 🚀">
+      <VCardText>This is your new campaign page.</VCardText>
+      <VCardText>
+        Welcome to the campaign dashboard! Here you can manage all your ongoing outreach efforts.
+      </VCardText>
+    </VCard>
+  </div>
+</template>
